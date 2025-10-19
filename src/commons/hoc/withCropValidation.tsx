e14f107d154b4301db3eff6bff0e19d0a8721a7c
pelix.png <semeonky@gmail.com>
2025-10-19 13:40:31 +0200

    feat: add onSwipeAtBaseZoom and fix type inference for HOC

--- src/commons/hoc/withCropValidation.tsx
+++ src/commons/hoc/withCropValidation.tsx
@@ -1,28 +1,24 @@
-import React, { forwardRef } from 'react';
-import { type CropZoomProps } from '../../components/crop/types';
+import React, { forwardRef } from 'react'
+import { type CropZoomProps } from '../../components/crop/types'
 
 export default function withCropValidation<T, P extends CropZoomProps>(
-  Component: React.ComponentType<P>
+  Component: React.ComponentType<P>,
 ) {
   return forwardRef<T, P>((props, ref) => {
-    const { minScale, maxScale } = props;
+    const { minScale, maxScale } = props
 
     if (minScale !== undefined && minScale < 1) {
-      throw new Error('minScale property must be greater than or equals one');
+      throw new Error('minScale property must be greater than or equals one')
     }
 
     if (maxScale !== undefined && maxScale < 1) {
-      throw new Error('maxScale property must be greater than or equals one');
+      throw new Error('maxScale property must be greater than or equals one')
     }
 
-    if (
-      minScale !== undefined &&
-      maxScale !== undefined &&
-      minScale > maxScale
-    ) {
-      throw new Error('minScale property must not be greater than maxScale');
+    if (minScale !== undefined && maxScale !== undefined && minScale > maxScale) {
+      throw new Error('minScale property must not be greater than maxScale')
     }
 
-    return <Component {...props} reference={ref} />;
-  });
+    return <Component {...(props as any)} reference={ref} />
+  })
 }
--- src/commons/hoc/withResumableValidation.tsx
+++ src/commons/hoc/withResumableValidation.tsx
@@ -1,42 +1,34 @@
-import React, { forwardRef } from 'react';
-import type { ResumableZoomProps } from '../../components/resumable/types';
+import React, { forwardRef } from 'react'
+import type { ResumableZoomProps } from '../../components/resumable/types'
 
-export default function withResumableValidation<
-  T,
-  P extends ResumableZoomProps
->(Component: React.ComponentType<P>) {
+export default function withResumableValidation<T, P extends ResumableZoomProps>(
+  Component: React.ComponentType<P>,
+) {
   return forwardRef<T, P>((props, ref) => {
-    const childrenCount = React.Children.count(props.children);
+    const childrenCount = React.Children.count(props.children)
     if (childrenCount !== 1) {
-      const message = `ResumableZoom expected one child but received ${childrenCount} children`;
-      throw new Error(message);
+      const message = `ResumableZoom expected one child but received ${childrenCount} children`
+      throw new Error(message)
     }
 
     if (props.minScale !== undefined && props.minScale < 1) {
-      throw new Error('minScale must be greater than or equals one');
+      throw new Error('minScale must be greater than or equals one')
     }
 
     if (typeof props.maxScale === 'number' && props.maxScale < 1) {
       throw new Error(
-        'maxScale must be greater than one, or a SizeVector object in order to infer the max scale'
-      );
+        'maxScale must be greater than one, or a SizeVector object in order to infer the max scale',
+      )
     }
 
-    if (
-      props.minScale &&
-      typeof props.maxScale === 'number' &&
-      props.minScale > props.maxScale
-    ) {
-      throw new Error('minScale must not be greater maxScale');
+    if (props.minScale && typeof props.maxScale === 'number' && props.minScale > props.maxScale) {
+      throw new Error('minScale must not be greater maxScale')
     }
 
-    if (
-      props.longPressDuration !== undefined &&
-      props.longPressDuration <= 250
-    ) {
-      throw new Error('longPressDuration must be greater than 250ms');
+    if (props.longPressDuration !== undefined && props.longPressDuration <= 250) {
+      throw new Error('longPressDuration must be greater than 250ms')
     }
 
-    return <Component {...props} reference={ref} />;
-  });
+    return <Component {...(props as any)} reference={ref} />
+  })
 }
